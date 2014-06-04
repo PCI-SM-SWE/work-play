49e28b4cbf139d71c8b5aa21c383d38caae69743
Tolga keskinoglu <tolga@umd.edu>
2014-06-04 14:12:24 -0400

    initial commit

--- redisServer.js
+++ redisServer.js
@@ -1,34 +1,36 @@
 var redis = require("redis"),
-    client = redis.createClient(null,"107.170.173.86",null);
+client = redis.createClient();
 var count = 0;
 client.on("error", function (err) {
-    console.log("Error " + err);
-    if (count === 4) {
-	console.log("Connection timeout");
-	client.end();
-	throw { name: 'Connection Timeout', message: 'Connecting to redis took too long' };
-    }
-    count = count + 1;
-    console.log("Connection timeout in: " + (4 - count));
+	console.log("Error " + err);
+	if (count === 4) {
+		console.log("Connection timeout");
+		client.end();
+		throw { name: 'Connection Timeout', message: 'Connecting to redis took too long' };
+	}
+	count = count + 1;
+	console.log("Connection timeout in: " + (4 - count));
 });
 
+console.dir(client.llen(users));
 function addUser (name) {
-    client.rpush("users", (client.llen("users") + 1));
-    client.hset("user:" + client.llen("users"), "name", name);
+	client.rpush("users", name);
+	client.hset("user:" + client.llen("users"), "name", name);
+	console.dir("I worked");
 }
 
 addUser("jimmy");
-
+console.dir(client.llen("users"));
 //client.hmset("user:1", "name", "tolga", "work", "5");
 //client.hmset("user:2", "name", "bill", "work", "8");
 
 //client.hgetall("user:1", function(err, response) {
-    //console.dir(response);
+//console.dir(response);
 //});
 
 for(var i = 0; i < 5; i++) {
-    client.hgetall("user:" + i, function(err, response) {
-        console.dir(response);
-    });
+	client.hgetall("user:" + i, function(err, response) {
+		console.dir(response);
+	});
 }
 client.quit();
