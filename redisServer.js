3e284d9cb3aeaf51deb15514b8350236dd1fb29d
Tolga keskinoglu <tolga@umd.edu>
2014-06-06 08:14:23 -0400

    redis server connection issue

--- redisServer.js
+++ redisServer.js
@@ -1,39 +1,51 @@
 var redis = require("redis"),
-client = redis.createClient();
-var count = 0;
+
+//throws error if 5 consecutive connections attempts are unsuccessful
+client = redis.createClient(null, null, {max_attempts:5});
+
 client.on("error", function (err) {
-	console.log("Error " + err);
-	if (count === 4) {
-		console.log("Connection timeout");
-		client.end();
-		throw { name: 'Connection Timeout', message: 'Connecting to redis took too long' };
-	}
-	count = count + 1;
-	console.log("Connection timeout in: " + (4 - count));
+	console.log(err);
 });
 
-
-function addUser (name) {
-	client.rpush("users", name);
-	client.llen('users', function(err, count){
-		client.hset("user:" + count, "name", name);
-		console.dir(count);
-	});
+if(!client.connected || !client.ping()) {
+	console.log("Client is not connected!!");
 }
 
-addUser("jimmy");
+//if(client.ping()) {
+//	console.dir('Connection established');
+//}
+//else {
+//	//throw { name: 'Error', message: 'connection was not established' };
+//}
+
+
+//function addUser (name) {
+//	client.rpush("users", name);
+//	client.llen('users', function(err, count){
+//		client.ping();
+//		client.hset("user:2", "name", "frank", redis.print);
+//		client.hset(("user:" + count), "name", name, redis.print);
+//	});
+//}
+//
+//addUser("jimmy");
+//client.hset("user:3", "name", "tolga");
 
 
 
 //client.hmset("user:1", "name", "tolga", "work", "5");
-//client.hmset("user:2", "name", "bill", "work", "8");
-
 //client.hgetall("user:1", function(err, response) {
 //console.dir(response);
 //});
-for(var i = 0; i < 5; i++) {
-	client.hgetall("user:" + i, function(err, response) {
-		console.dir(response);
-	});
-}
+
+
+
+//for(var i = 1; i < 5; i++) {
+//	client.hgetall("user:" + i, function(err, response) {
+//		console.dir(response);
+//	});
+//}
+//client.llen('users', function(err, ret){
+//	console.dir(ret);
+//});
 client.quit();
