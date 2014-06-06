3812ffee83f92dd706fdb8f9d9c1d58792c365fa
humphrey8146 <humphrey8146@gmail.com>
2014-06-06 15:48:49 -0400

    commit(Humphrey)

--- public/app_ui.js
+++ public/app_ui.js
@@ -5,20 +5,74 @@ var application;
 
 app.controller ('mainCtrl', function ($scope)
 {
+	function sort (array)
+	{
+		var i;
+		var j;
+		var largestIndex;
+		
+		for (i = 0; i < array.length; i++)
+		{			
+			largestIndex = i;
+			
+			for (j = i + 1; j < array.length; j++)
+			{
+				if (parseInt(array[j].points) > parseInt(array[largestIndex].points))
+					largestIndex = j;
+			}
+			
+			var temp = array[i];
+			array[i] = array[largestIndex];
+			array[largestIndex] = temp;
+		}
+		
+		return (array);		
+	}
+	
 	$(document).ready (function ()
 	{	
 		application = new application (socket);	
+		application.getScoreboard ();
 		
 		socket.on ('error message', function (err)
 		{
 			alert (err);				
 		});
 		
-		socket.on ('scoreboard', function (parameters)
+		socket.on ('scoreboardData', function (parameters)
 		{
-			//append to $('#scoreboardBody')
+			$('#scoreboardBody').empty ();
+			
+			var array = parameters;
+			var newArray = new Array ();
+			var i;
+						
+			for (i = 0; i < array.length; i++)
+			{
+				newArray.push (array[i].person);
+			}
+			
+			array = sort (newArray);
+			
+			for (i = 0; i < array.length; i++)
+			{
+				console.log (array[i].name + " " + array[i].points);
+				var tr = document.createElement ('tr');
+				var td1 = document.createElement ('td');
+				td1.innerHTML = i + 1;
+				var td2 = document.createElement ('td');
+				td2.innerHTML = array[i].name;
+				var td3 = document.createElement ('td');
+				td3.innerHTML = array[i].points;
+				tr.appendChild (td1);
+				tr.appendChild (td2);
+				tr.appendChild (td3);
+				$('#scoreboardBody').append (tr);				
+			}			
 		});
 	});		
+	
+	
 	
 	
 	$scope.activities = ['',
@@ -61,10 +115,10 @@ app.controller ('mainCtrl', function ($scope)
 		var user = $('#user').text ();
 		var firstName = user.substring (0, user.indexOf (' '));
 		user = user.substr (user.indexOf (' ') + 1);
-		var lastName = user.substring (0, user.indexOf (' '));
+		var lastName = user.substring (0, user.indexOf ('('));
 		
-		alert (firstName + "\n" + lastName + "\n" + activity);
-		application.sendData (firstName, lastName, activity);
+		//alert (firstName + "\n" + lastName + "\n" + activity);
+		application.submitActivity (firstName, lastName, activity);
 		//finish
 	};	
 });
--- public/application.js
+++ public/application.js
@@ -3,9 +3,14 @@ var application = function (socket)
 	this.socket = socket;
 };
 
-application.prototype.sendData = function (firstName, lastName, activity)
+application.prototype.getScoreboard = function ()
+{
+	this.socket.emit ("getScoreboard");
+};
+
+application.prototype.submitActivity = function (firstName, lastName, activity)
 {
 	var parameters = {'firstName': firstName, 'lastName': lastName, 'activity': activity};
-	this.socket.emit ("sendData", parameters);
+	this.socket.emit ("submitActivity", parameters);
 };
 
